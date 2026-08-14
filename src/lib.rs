e7604f909cc9c44a24b8bca5deebe63223275579
Denched <166449459+Denched@users.noreply.github.com>
2026-08-14 19:04:12 +0100

    Cleanup lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,12 @@ pub enum Instructions {
     Split(usize, usize),
     Match,
 }
-///
+
 /// Recursive function utilized to lazily add threads to the list and sets the base case as either CHAR or MATCH
 ///
-///
 /// e.g
-/// add_thread(&mut cList,&mut visited, 0, &program);
-/// add_thread(&mut nList,&mut visited, pc + 1, &program);
-
+/// add_thread(&mut c_list, &mut visited, 0, &program);
+/// add_thread(&mut n_list, &mut visited, pc + 1, &program);
 fn add_thread(
     list: &mut Vec<usize>,
     visited: &mut Vec<usize>,
@@ -37,22 +35,23 @@ fn add_thread(
         }
     }
 }
+
 pub fn run(program: &[Instructions], input: &str) -> bool {
-    let mut cList: Vec<usize> = vec![]; // threads for curr char
-    let mut nList: Vec<usize> = vec![]; // threads being built for next char
+    let mut c_list: Vec<usize> = vec![]; // threads for curr char
+    let mut n_list: Vec<usize> = vec![]; // threads being built for next char
 
     {
         let mut visited: Vec<usize> = vec![];
 
-        // Add initial pc(s) in cList at the very start of the program to process later
-        add_thread(&mut cList, &mut visited, 0, &program);
+        // Add initial pc(s) in c_list at the very start of the program to process later
+        add_thread(&mut c_list, &mut visited, 0, program);
     }
     // sp = current pos in the string
     for sp in 0..=input.len() {
         let mut visited: Vec<usize> = vec![];
 
         // Check if atleast one active thread in the execution list has reached MATCH
-        if cList
+        if c_list
             .iter()
             .any(|&pc| matches!(program[pc], Instructions::Match))
         {
@@ -60,18 +59,18 @@ pub fn run(program: &[Instructions], input: &str) -> bool {
         }
         // PC is an index into the Vec to indicate the instruction we are currently on
 
-        for &pc in &cList {
-            if let Instructions::Char(expected) = program[pc] {
-                if input.as_bytes().get(sp) == Some(&expected) {
-                    add_thread(&mut nList, &mut visited, pc + 1, &program);
-                }
+        for &pc in &c_list {
+            if let Instructions::Char(expected) = program[pc]
+                && input.as_bytes().get(sp) == Some(&expected)
+            {
+                add_thread(&mut n_list, &mut visited, pc + 1, program);
             }
         }
-        // Iteration done, update cList, empty out nList to prepare for the next iteration.
-        cList = nList;
-        nList = vec![];
+        // Iteration done, update c_list, empty out n_list to prepare for the next iteration.
+        c_list = n_list;
+        n_list = vec![];
 
-        if cList.is_empty() {
+        if c_list.is_empty() {
             println!("No match found");
             return false;
         }
